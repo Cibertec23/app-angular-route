6bf047fff28785a1fb504a58d164993ad8429794
cuartociclo2023 <cuartocicloidat2023@gmail.com>
2023-11-18 19:39:48 -0500

    ruteo de componentes base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './angular-material/material/material.module';
+import { LoginComponent } from './auth/login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './dashboard/home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { CursoListaComponent } from './dashboard/curso/curso-lista/curso-lista.component';
+import { CursoDetalleComponent } from './dashboard/curso/curso-detalle/curso-detalle.component';
+import { ProductoListaComponent } from './dashboard/producto/producto-lista/producto-lista.component';
+import { ProductoDetalleComponent } from './dashboard/producto/producto-detalle/producto-detalle.component';
+import { CategoriaListaComponent } from './dashboard/categoria/categoria-lista/categoria-lista.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    HomeComponent,
+    PageNotFoundComponent,
+    CursoListaComponent,
+    CursoDetalleComponent,
+    ProductoListaComponent,
+    ProductoDetalleComponent,
+    CategoriaListaComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    RouterModule.forRoot([
+      {path: 'login', component: LoginComponent},
+      {path: '', redirectTo: 'login', pathMatch: 'full'},
+      {path: '**', component: PageNotFoundComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
