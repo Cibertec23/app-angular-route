443b4a8c1f325605df267d5f18c21c9d8963eac6
cuartociclo2023 <cuartocicloidat2023@gmail.com>
2023-11-18 20:05:41 -0500

    ruteo de hijos

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+
+  constructor(private route: ActivatedRoute, private router: Router){
+
+  }
+
+  onNavigateToHomePage(): void{
+    this.router.navigate(['home'], {relativeTo: this.route});
+  }
+
+  onNavigateToProductPage(): void{
+    this.router.navigate(['producto'], {relativeTo: this.route});
+  }
+
+  onNavigateToCoursePage(): void{
+    this.router.navigate(['curso'], {relativeTo: this.route});
+  }
 
 }
